9d43eb7b2afb7b2d0884a1259973248290266e7b
oscarorangelt <89954606+oscarorangelt@users.noreply.github.com>
2024-07-10 19:03:21 -0500

    commented pip install requirements in build sh

--- build.sh
+++ build.sh
@@ -6,7 +6,7 @@ set -o errexit
 pip install -r requirements.txt
 
 #poetry install
-pip install -r requirements.txt
+#pip install -r requirements.txt
 
 # Convert static asset files
 python manage.py collectstatic --no-input
