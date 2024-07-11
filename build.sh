0ec568afac3f62d123413c7662329a05dbe7fc1b
oscarorangelt <89954606+oscarorangelt@users.noreply.github.com>
2024-07-10 23:43:04 -0500

    quitando comando requirements repetido

--- build.sh
+++ build.sh
@@ -2,9 +2,6 @@
 # Exit on error
 set -o errexit
 
-# Modify this line as needed for your package manager (pip, poetry, etc.)
-pip install -r requirements.txt
-
 #poetry install
 pip install -r requirements.txt
 
